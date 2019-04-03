ebb5c2809f712306c880d5c7d26a42193417dfc9
idealcn <gn18667130631@126.com>
2019-04-03 23:10:44 +0800

    修改LinearIndicator

--- app/src/main/java/com/idealcn/event/study/widget/indicator/LinearIndicator.java
+++ app/src/main/java/com/idealcn/event/study/widget/indicator/LinearIndicator.java
@@ -5,13 +5,12 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewConfiguration;
 
 import com.idealcn.event.study.R;
 
@@ -23,7 +22,7 @@ public class LinearIndicator extends View {
     //是否可以水平滚动
     public static final int SCROLL_MODE_SCROLLABLE = 0;
     public static final int SCROLL_MODE_FIXED = 1;
-    private    int mScrollModel = SCROLL_MODE_FIXED;
+    private int mScrollModel = SCROLL_MODE_FIXED;
 
     //下方的指示线条
     private Paint mIndicatorLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -46,14 +45,15 @@ public class LinearIndicator extends View {
 
     private List<String> titleArray = new ArrayList<>();
 
-    private ViewDragHelper dragHelper;
+
+    private         int scaledTouchSlop;
 
     public LinearIndicator(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public LinearIndicator(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public LinearIndicator(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -61,20 +61,20 @@ public class LinearIndicator extends View {
         TypedArray typedArray = null;
         try {
             typedArray = context.obtainStyledAttributes(attrs, R.styleable.LinearIndicator);
-            mIndicatorLineWidth =  typedArray.getDimensionPixelSize(R.styleable.LinearIndicator_indicator_line_width,60);
-            mIndicatorLineHeight =  typedArray.getDimensionPixelSize(R.styleable.LinearIndicator_indicator_line_height,10);
-            mIndicatorLineColor = typedArray.getColor(R.styleable.LinearIndicator_indicator_line_color,Color.BLUE);
-            mDefaultTextColor = typedArray.getColor(R.styleable.LinearIndicator_indicator_text_color,Color.BLUE);
-            mTextSize =  typedArray.getDimensionPixelSize(R.styleable.LinearIndicator_indicator_text_size,16);
-            mScrollModel = typedArray.getInteger(R.styleable.LinearIndicator_indicator_scroll_mode,SCROLL_MODE_SCROLLABLE);
+            mIndicatorLineWidth = typedArray.getDimensionPixelSize(R.styleable.LinearIndicator_indicator_line_width, 60);
+            mIndicatorLineHeight = typedArray.getDimensionPixelSize(R.styleable.LinearIndicator_indicator_line_height, 10);
+            mIndicatorLineColor = typedArray.getColor(R.styleable.LinearIndicator_indicator_line_color, Color.BLUE);
+            mDefaultTextColor = typedArray.getColor(R.styleable.LinearIndicator_indicator_text_color, Color.BLUE);
+            mTextSize = typedArray.getDimensionPixelSize(R.styleable.LinearIndicator_indicator_text_size, 16);
+            mScrollModel = typedArray.getInteger(R.styleable.LinearIndicator_indicator_scroll_mode, SCROLL_MODE_SCROLLABLE);
             CharSequence[] textArray = typedArray.getTextArray(R.styleable.LinearIndicator_indicator_text);
             for (CharSequence sequence : textArray) {
                 titleArray.add(sequence.toString());
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            if (null!=typedArray){
+        } finally {
+            if (null != typedArray) {
                 typedArray.recycle();
             }
         }
@@ -88,8 +88,7 @@ public class LinearIndicator extends View {
         mIndicatorTextPaint.setStyle(Paint.Style.STROKE);
         mIndicatorTextPaint.setTextSize(mTextSize);
 
-
-
+        scaledTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
     }
 
     //这里宽高由父ViewGroup负责测量
@@ -106,8 +105,8 @@ public class LinearIndicator extends View {
 //        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 //        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
-        if(mScrollModel==SCROLL_MODE_FIXED){
-          mIndicatorLineWidth =  getMeasuredWidth()/titleArray.size();
+        if (mScrollModel == SCROLL_MODE_FIXED) {
+            mIndicatorLineWidth = getMeasuredWidth() / titleArray.size();
         }
 
     }
@@ -121,48 +120,38 @@ public class LinearIndicator extends View {
         for (int x = 0; x < size; x++) {
             text = titleArray.get(x);
             float mTextWidth = mIndicatorTextPaint.measureText(text);
-            canvas.drawText(text,mIndicatorLineWidth*x + mIndicatorLineWidth/2 - mTextWidth/2,(getHeight()-mIndicatorLineHeight)/2+5,mIndicatorTextPaint);
+            canvas.drawText(text, mIndicatorLineWidth * x + mIndicatorLineWidth / 2 - mTextWidth / 2, (getHeight() - mIndicatorLineHeight) / 2 + 5, mIndicatorTextPaint);
         }
 
-        canvas.drawRect(0,getHeight() - mIndicatorLineHeight,mIndicatorLineWidth,getHeight(),mIndicatorLinePaint);
+        canvas.drawRect(0, getHeight() - mIndicatorLineHeight, mIndicatorLineWidth, getHeight(), mIndicatorLinePaint);
     }
+
+    VelocityTracker velocityTracker;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        dragHelper.processTouchEvent(event);
+        if (null == velocityTracker) {
+            velocityTracker = VelocityTracker.obtain();
+            velocityTracker.addMovement(event);
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                velocityTracker.computeCurrentVelocity(1000, 100);
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                break;
+            case MotionEvent.ACTION_CANCEL:
+
+                break;
+        }
         return true;
     }
 
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        dragHelper =ViewDragHelper.create((ViewGroup) this.getParent(), new ViewDragHelper.Callback() {
-            @Override
-            public boolean tryCaptureView(@NonNull View child, int pointerId) {
-
-                return true;
-            }
-
-            @Override
-            public void onViewPositionChanged(@NonNull View changedView, int left, int top, int dx, int dy) {
-                super.onViewPositionChanged(changedView, left, top, dx, dy);
-            }
-
-            @Override
-            public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
-                super.onViewReleased(releasedChild, xvel, yvel);
-            }
-
-            @Override
-            public int clampViewPositionHorizontal(@NonNull View child, int left, int dx) {
-                if (mScrollModel == SCROLL_MODE_FIXED)return 0;
-                if (mScrollModel == SCROLL_MODE_SCROLLABLE){
-                    return titleArray.size() * mIndicatorLineWidth -  getWidth();
-                }
-                return super.clampViewPositionHorizontal(child, left, dx);
-            }
-
-        });
     }
 
     @Override
